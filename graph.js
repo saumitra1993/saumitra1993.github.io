e6aa80dadf83d81ae212aecba1db619b63bdf5df
saumitra1993 <theapogee2011@gmail.com>
2018-12-01 15:35:27 +0530

    redo tree structure

--- graph.js
+++ graph.js
@@ -39,17 +39,17 @@ var cy = cytoscape({
       { data: { id: '0' } }
     ],
     edges: [
-      { data: { source: '10', target: '9' } },
-      { data: { source: '9', target: '8' } },
-      { data: { source: '8', target: '7' } },
-      { data: { source: '7', target: '6' } },
-      { data: { source: '6', target: '5' } },
-      { data: { source: '7', target: '4' } },
-      { data: { source: '4', target: '2' } },
-      { data: { source: '3', target: '1' } },
-      { data: { source: '4', target: '1' } },
-      { data: { source: '5', target: '3' } },
-      { data: { source: '1', target: '0' } }
+      { data: { source: '9' , target:'10'} },
+      { data: { source: '8', target: '9'} },
+      { data: { source: '7', target: '8'} },
+      { data: { source: '6', target: '7'} },
+      { data: { source: '5', target: '6'} },
+      { data: { source: '4', target: '7'} },
+      { data: { source: '2', target: '4'} },
+      { data: { source: '1', target: '3'} },
+      { data: { source: '1', target: '4'} },
+      { data: { source: '3', target: '5'} },
+      { data: { source: '0', target: '1'} }
     ]
   },
 
@@ -78,6 +78,24 @@ cy.$('#0').qtip({
     at: 'bottom center',
     target: 'mouse'
   },
+  show: {
+        target: false, // Defaults to target element
+        event: 'mouseenter', // Show on mouse over by default
+        effect: true, // Use default 90ms fade effect
+        delay: 90, // 90ms delay before showing
+        solo: false, // Do not hide others when showing
+        ready: false, // Do not show immediately
+    },
+    hide: {
+        target: false, // Defaults to target element
+        event: 'mouseleave', // Hide on mouse out by default
+        effect: true, // Use default 90ms fade effect
+        delay: 0, // No hide delay by default
+        fixed: false, // Non-hoverable by default
+        inactive: false, // Do not hide when inactive
+        leave: 'window', // Hide when we leave the window
+        distance: false // Don't hide after a set distance
+    },
   style: {
     classes: 'qtip-bootstrap'
   }
